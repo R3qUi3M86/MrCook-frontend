1d56d9b9d83d36d30cc04bc6e563e43a8fea1b67
R3qUi3M86 <requiem123@gmail.com>
2022-08-06 13:08:12 +0200

    optimized model loader, fixed memory leak

--- src/components/pages/product/3dmodel/ProductModel.js
+++ src/components/pages/product/3dmodel/ProductModel.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from "react";
-import { MeshLambertMaterial, MeshPhongMaterial } from "three";
+import React from "react";
+import { MeshPhongMaterial } from "three";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Suspense } from "react";
+import { useLoader } from "react-three-fiber";
 
 const path = process.env.PUBLIC_URL;
 const scenePath = path + '/scene/scene.gltf'
 
 const ProductModel = () => {
     const newMaterial = new MeshPhongMaterial({color: 0x5e3440, shininess:83});
-    const [model, setModel] = useState();
-    useEffect(() => {
-        new GLTFLoader().load(scenePath, setModel)
-        if (model){
-            model.scene.traverse((o) => {if (o.isMesh) o.material = newMaterial;})
-        }
-    })
-    
-    
-    
+    const gltf = useLoader(GLTFLoader, scenePath)
+    if (gltf) {
+        gltf.scene.traverse((o) => {
+            if (o.isMesh) o.material = newMaterial;
+        });
+    }
     return (
-        model ? <primitive object={model.scene}/> : null
-    )
+        <Suspense fallback={null}>
+            <primitive object={gltf.scene} />
+        </Suspense>
+  )
 }
 
 export default ProductModel
